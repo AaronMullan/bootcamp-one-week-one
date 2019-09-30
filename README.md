55a6fb64a021d2b3410fb384fe353fbd2c468fdb
AaronMullan <aaroncmullan@gmail.com>
2019-09-30 16:56:00 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# bootcamp-one-week-one
+
+### About Me Page
+
+**bold**
+*italics*
+
+-About
+_List
+_of
+  -subset
+  -of
+  -sub
+  -things
+_Things
---
+++ app.js
@@ -0,0 +1,20 @@
+import {isCorrect} from 'functions.js';
+
+const myButton = document.getElementById('action-button');
+
+myButton.onclick = () => {
+    console.log('it happened');
+    alert('welcome to my quiz!');
+    const myConfirmation = confirm('do you wanna take my quiz?');
+    console.log(myConfirmation);
+    const userAnswer = prompt('is my favorite color bluish');
+    const sanitizedUserAnswer = userAnswer.toLocaleLowerCase().trim();
+    console.log(sanitizedUserAnswer);
+
+    const
+
+    if(sanitizedUserAnswer === 'yes'){
+        ('right-o!');}
+    else {alert('naah');
+    }
+};
